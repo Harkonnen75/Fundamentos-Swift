f4de2d473e30b5b956c7f9f35f4b44e432a47dfb
Mati <alexcastanr@hotmail.com>
2019-10-18 13:04:50 +0200

    Initial Commit

---
+++ Projecto6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projecto6
+//
+//  Created by mati on 18/10/2019.
+//  Copyright © 2019 mati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
